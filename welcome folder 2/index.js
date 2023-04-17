15bb904fa8490415ded0f084150c5e732fb1190c
Ishaan <ishaanguleria8988@gmail.com>
2023-04-17 09:51:45 +0530

    Done

---
+++ welcome folder 2/index.js
@@ -0,0 +1,25 @@
+
+function LogEvent(){
+    var login = document.getElementById("login");
+    var signup = document.getElementById("signup");
+    login.style.width="60%";
+    login.style.fontWeight="bold";
+    login.style.background = "#565ABB";
+
+    signup.style.width = "40%";
+    signup.style.fontWeight="normal";
+    signup.style.background = "black"
+
+}
+function SignEvent(){
+    var login = document.getElementById("login");
+    var signup = document.getElementById("signup");
+    signup.style.width="60%";
+    signup.style.fontWeight="bold";
+    signup.style.background = "#565ABB";
+
+    login.style.width = "40%";
+    login.style.fontWeight="normal";
+    login.style.background = "black"
+
+}
